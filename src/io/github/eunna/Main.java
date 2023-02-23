0cf54ab2b5616164221d55d63b16cbcf81facd48
chltks0710 <dmssk0226@!>
2023-02-24 04:55:01 +0900

    Update Project

---
+++ src/io/github/eunna/Main.java
@@ -0,0 +1,14 @@
+package io.github.eunna;
+
+
+public class Main {
+
+
+    public static void main(String[] args) {
+
+        int a = 30;
+
+        System.out.println(a);
+        System.out.println();
+    }
+}
---
+++ src/io/github/eunna/Test.java
@@ -0,0 +1,15 @@
+package io.github.eunna;
+
+public class Test {
+
+    public static void main(String[] args) {
+        // 문자 | char
+        // 문자열 | String
+        // 정수 | byte short int long
+        // 실수 | float double
+        // 논리형 | boolean
+
+
+
+    }
+}
